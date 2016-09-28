b6d4b60656161d1cd1e168d39247937d737c4bd5
akshat <akshatdubey@outlook.com>
2016-09-28 12:24:40 +0530

    added check for all permutations of opposite words

--- oppgame.py
+++ oppgame.py
@@ -15,12 +15,14 @@ for n in xrange(2, (len(input_word)/2) + 1):
 		perm = ''.join(perm)
 	
 		if perm in words:
-			wrd_cpy = input_word[:]
+			input_word_copy = input_word[:]
 			
 			# remove the first occurrences of the letters
 			for ch in perm:
-				wrd_cpy = wrd_cpy.replace(ch, '', 1)
+				input_word_copy = input_word_copy.replace(ch, '', 1)
 			
-			opp_perm_list = 
-			if wrd_cpy in words:
-				print perm, wrd_cpy
+			opp_perm_list = list(permutations(input_word_copy))
+			for opp_perm in opp_perm_list:
+				opp_perm = ''.join(opp_perm)
+				if opp_perm in words:
+					print perm, opp_perm
