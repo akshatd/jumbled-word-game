cf3526111e5082f3cb513551274b886375b87e60
akshat <akshatdubey@outlook.com>
2016-09-28 12:57:40 +0530

    changed data struct to set, much faster executions

--- oppgame.py
+++ oppgame.py
@@ -2,13 +2,15 @@ from itertools import permutations
 
 # import all the words
 wordfile = open("words.txt")
-words = list(x.rstrip() for x in wordfile)
+words = set(x.rstrip() for x in wordfile)
+wordfile.close()
 
 # take input
 input_word = raw_input("Enter the word to be checked: ").lower()
 
 # find all permutations that can be checked
 for n in xrange(2, (len(input_word)/2) + 1):
+	print n
 	perm_list = (list(permutations(input_word, n)))
 	
 	for perm in perm_list:
@@ -17,6 +19,7 @@ for n in xrange(2, (len(input_word)/2) + 1):
 		if perm in words:
 			input_word_copy = input_word[:]
 			
+			# print "current word: ", perm
 			# remove the first occurrences of the letters
 			for ch in perm:
 				input_word_copy = input_word_copy.replace(ch, '', 1)
